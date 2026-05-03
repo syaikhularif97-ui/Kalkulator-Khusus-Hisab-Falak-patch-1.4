bdcfffedaab5dcf6e971718b3565d0dc7a388635
syaikhularif97-ui <syaikhularif97@gmail.com>
2026-05-03 22:11:48 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,33 @@
+const cacheName = "kalkulator-v1";
+const assets = [
+  "index.html",
+  "manifest.json",
+  "sw.js",
+  "icon.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});sw
